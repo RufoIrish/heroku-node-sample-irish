9ca025b15cb9a0d56ae01c5920ddadc9e56e8a7f
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-25 13:55:57 +0800

    added port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var port = process.env.port || 5000
+var port = process.env.PORT || 5000
 
 app.get('/', function(req, res) {
     res.send('<h1>Hello world</h1>');
